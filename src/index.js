b37551acd6433d860d6ddeb977ad1b834857a667
farya <ftjamal2017@gmail.com>
2022-09-15 07:55:01 +0500

    done

--- src/index.js
+++ src/index.js
@@ -10,31 +10,33 @@ import shopImg from "./img/tshop.png";
 import post2Img from "./img/post2.jpg";
 import './index.css';
 
-const InstaCard = ({ profile, name, faEllipsisVertical, faHeart, faComment, faBookmark, faPaperPlane, imageUrl, likes, text, pageName, comment, date }) => (
+const InstaCard = ({ profile, name, imageUrl, likes, text, pageName, comment, date }) => (
   <div className='body'>
     <div className='header'>
       <p className='intro'>
         <img class="profile" src={profile} alt="" /> <h3 className='name'>{name}</h3>
       </p>
-
-      <p className='dots'> <FontAwesomeIcon icon={faEllipsisVertical} /></p>
+      <p className='dots'><FontAwesomeIcon icon={faEllipsisVertical} /></p>
     </div>
 
     <img className='img' src={imageUrl} alt="" />
     <p className='icons'>
       <div className='interact'>
         <span className='heart'><FontAwesomeIcon icon={faHeart} /></span>
-        <span className='heart'><FontAwesomeIcon icon={faComment} /></span>
+        <span className='heart'><FontAwesomeIcon icon={faComment} flip="horizontal" /></span>
         <span className='heart'><FontAwesomeIcon icon={faPaperPlane} /></span>
       </div>
       <span className='save'><FontAwesomeIcon icon={faBookmark} /></span>
     </p>
+
     <h4>{likes}</h4>
     <p>
       <span><b>{pageName}</b></span> <span>{text}</span>
     </p>
     <p className='grey'>{comment}</p>
     <p className='grey'>{date}</p>
+    
+
     <br />
   </div>
 )
@@ -43,11 +45,6 @@ const InstaPage = () => (
     <InstaCard
       profile={profile}
       name="Farya Jamal"
-      faEllipsisVertical={faEllipsisVertical}
-      faHeart={faHeart}
-      faComment={faComment}
-      faPaperPlane={faPaperPlane}
-      faBookmark={faBookmark}
       imageUrl={postImg}
       likes="100 likes"
       text="hAPPY Independence Day"
@@ -58,11 +55,6 @@ const InstaPage = () => (
     <InstaCard
       profile={shopImg}
       name="Toy Mart"
-      faEllipsisVertical={faEllipsisVertical}
-      faHeart={faHeart}
-      faComment={faComment}
-      faPaperPlane={faPaperPlane}
-      faBookmark={faBookmark}
       imageUrl={toyImg}
       likes="100 likes"
       text="Stock Alert!"
@@ -73,11 +65,6 @@ const InstaPage = () => (
     <InstaCard
       profile={bookImg}
       name="Book Shop"
-      faEllipsisVertical={faEllipsisVertical}
-      faHeart={faHeart}
-      faComment={faComment}
-      faPaperPlane={faPaperPlane}
-      faBookmark={faBookmark}
       imageUrl={post2Img}
       likes="100 likes"
       text="Read what you want, where you"
